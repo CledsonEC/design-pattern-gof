5fdbe6bd9f979cea50ec25b419f315edec585923
Cledson <cledson.caetano@gmail.com>
2019-01-23 20:10:19 -0200

    Implementação do padrão Singleton

---
+++ gof/ConsoleApp1/Config.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Singleton
+{
+    public class Config
+    {
+        private static Config conf = null;
+
+        public string paramGlobal { get; set; }
+
+        private Config()
+        {
+
+        }
+
+        public static Config GetInstance() {
+
+            if (conf == null)
+                conf = new Config();
+
+            return conf;
+        }
+
+    }
+}
---
+++ gof/ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Config conf1 = Config.GetInstance();
+
+            conf1.paramGlobal = "Param 1";
+            Console.WriteLine(conf1.paramGlobal);
+
+            Config conf2 = Config.GetInstance();
+
+            conf2.paramGlobal = "Param 2";
+            Console.WriteLine(conf2.paramGlobal);
+            Console.WriteLine(conf1.paramGlobal);
+
+            Console.ReadKey();
+        }
+    }
+}
